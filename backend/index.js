34d39db774cf4fd47f38c0208c1c85f80da003aa
Tanmay <tanmaywadkar7@gmail.com>
2021-09-13 13:46:45 +0530

    44 Creating Routes & Schema for Storing Notes and Users

--- backend/index.js
+++ backend/index.js
@@ -10,6 +10,10 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+//Available Routes
+app.use('/api/auth',require('./routes/auth'))
+app.use('/api/notes',require('./routes/notes'))
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
---
+++ backend/routes/auth.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const router = express.Router();
+
+router.get('/',(req,res)=>{
+   obj = { 
+    a: 'Tanmay',
+    number: 34
+
+   }
+   res.json(obj)
+
+
+})
+
+module.exports = router
