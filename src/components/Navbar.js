ea9a323bffe01e27e077ecc66269b6949947e679
Tanmay <tanmaywadkar7@gmail.com>
2021-09-23 21:23:22 +0530

    V72 Fetching User specific details in iNotebook

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -12,7 +12,7 @@ const Navbar = () => {
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
         <div className="container-fluid">
-          <Link className="navbar-brand" to="/">Cloud Note Book</Link>
+          <Link className="navbar-brand" to="/">iNoteBook</Link>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
